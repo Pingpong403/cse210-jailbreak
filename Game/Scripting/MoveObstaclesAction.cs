446906ca9595a4d5f57dcfc6e636fb4181c658ab
torgill43 <timorgill43@gmail.com>
2022-12-09 13:46:22 -0700

    Object wrapping

--- Game/Scripting/MoveObstaclesAction.cs
+++ Game/Scripting/MoveObstaclesAction.cs
@@ -1,14 +1,22 @@
 using Unit06.Game.Casting;
+using Unit06.Game.Services;
 using System.Collections.Generic;
+using System;
 
 namespace Unit06.Game.Scripting
 {
     public class MoveObstaclesAction : Action
     {
+        
         public MoveObstaclesAction()
         {
         }
 
+        /// <summary>
+        /// Moves the actor to its next position according to its velocity. Will wrap the position 
+        /// from one side of the screen to the other when it reaches the maximum x and y 
+        /// values.
+        /// </summary>
         public void Execute(Cast cast, Script script, ActionCallback callback)
         {
             List<Actor> obstacles = cast.GetActors(Constants.OBSTACLE_GROUP);
@@ -16,12 +24,22 @@ namespace Unit06.Game.Scripting
 
             foreach (Obstacle obstacle in obstacles)
             {
+                int maxX = Constants.SCREEN_WIDTH;
+                int maxY = Constants.SCREEN_HEIGHT;
+
                 Body body = obstacle.GetBody();
                 Point position = body.GetPosition();
                 Point velocity = body.GetVelocity();
-                position = position.Add(velocity);
+
+                int x = ((position.GetX() + velocity.GetX()) + maxX) % maxX;
+                int y = ((position.GetY() + velocity.GetY()) + maxY) % maxY;
+                position = new Point(x, y);
+
+                // position = position.Add(velocity);
                 body.SetPosition(position);
             }
+
+        
         }
     }
 }
